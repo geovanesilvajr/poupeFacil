8ed53e05800af7d6cde308c1db786f3ac4b322d0
geovanesilvajr <geovane.silvajr@gmail.com>
2026-02-09 20:15:27 -0300

    feat: configura tailwind css e arquivos

--- frontend/react/poupeFacil/src/App.jsx
+++ frontend/react/poupeFacil/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Hello World");
+
   return (
     <>
-      <div>Hello World</div>
+      <h1 className="text-3xl font-bold underline text-center">{message}</h1>
+      <button
+        className="  bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block mx-auto mt-4"
+        onClick={() => setMessage("Hello React!")}
+      >
+        Click me
+      </button>
     </>
   );
 }
